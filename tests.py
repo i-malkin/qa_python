890108d773190dc56310cc0541a3762bc880c7ca
Malkin Ilya <i-malkin@ya.ru>
2024-03-27 21:44:21 +0400

    final_

--- tests.py
+++ tests.py
@@ -8,6 +8,19 @@ class TestBooksCollector:
         collector.add_new_book('Книга_2')
         assert len(collector.get_books_genre()) == 2
 
+    # проверка добавления двух одинаковых книг в словарь ==============================
+    def test_add_new_book_add_two_copy_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Горе от ума')
+        collector.add_new_book('Горе от ума')
+        assert len(collector.get_books_genre()) == 1
+
+    # проверка добавления книги с длинной имени больше 40 символов
+    def test_add_new_book_add_book_with_name_50_symbol(self):
+        collector = BooksCollector()
+        collector.add_new_book('Long_Name_Long_Name_Long_Name_Long_Name_Long_Name_')
+        assert len(collector.get_books_genre()) == 0
+
     # проверка присвоения допустимых жанров для книги из списка ==========================
     @pytest.mark.parametrize('genre',['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
     def test_set_book_genre_for_book_in_list(self, genre):
@@ -31,4 +44,26 @@ class TestBooksCollector:
         collector.set_book_genre('Книга_5', 'Комедии')
         assert collector.get_books_with_specific_genre('Комедии') == ['Книга_5']
 
-    #
+    # проверка добавления двух книг в избранное  ========================================
+    def test_add_book_in_favorites_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга_6')
+        collector.add_new_book('Книга_7')
+        collector.add_book_in_favorites('Книга_6')
+        collector.add_book_in_favorites('Книга_7')
+        assert len(collector.get_list_of_favorites_books()) == 2
+
+    # проверяем добавление книги в избранное, которой нет в словаре
+    def test_add_book_in_favorites_not_in_list(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Книга_8')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    # проверка удаления книги из избранного, если она там есть
+    def test_delete_book_from_favorites_if_book_in_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Книга_9')
+        collector.add_book_in_favorites('Книга_9')
+        collector.delete_book_from_favorites('Книга_9')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
